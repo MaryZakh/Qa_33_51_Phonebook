e7662cc9800dcdea9ab474f5a79bab5bbe71ba2f
Maria Z <z0559882272@gmail.com>
2026-04-30 20:58:23 +0300

    added loginSuccess without Asserts

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,21 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
   @Test
-  public void test (){
+  public void loginSuccess (){
+   app.getHelperUser().openLoginRegistrationForm();
+   app.getHelperUser().fillLoginRegistrationForm("margo@gmail.com","Mmar123456$");
+   app.getHelperUser().submitLogin();
 
+   //Assert
+//      Assert.assertEquals();
+//      Assert.assertNotEquals();
+//      Assert.assertTrue();
+//      Assert.assertFalse();
   }
 
 }
