768105ffd5d1520eff0e367edd852a7e83fe919b
Maria Z <z0559882272@gmail.com>
2026-05-04 20:35:19 +0300

    added LoginTests negative

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,8 +1,13 @@
 package manager;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HelperUser extends HelperBase {
 
@@ -22,25 +27,40 @@ public class HelperUser extends HelperBase {
 //        emailInput.click();
 //        emailInput.clear();
 //        emailInput.sendKeys(email);
-        type(By.name("email"),email);
+        type(By.name("email"), email);
 
 
 //        WebElement passwordInput = wd.findElement(By.xpath("//input[@placeholder='Password']"));
 //        passwordInput.click();
 //        passwordInput.clear();
 //        passwordInput.sendKeys(password);
-        type(By.xpath("//input[@placeholder='Password']"),password);
+        type(By.xpath("//input[@placeholder='Password']"), password);
     }
 
-    public void submitLogin(){
+    public void submitLogin() {
         click(By.xpath("//button[text()='Login']"));
     }
 
     public boolean isLogged() {
-       return isElementPresent(By.xpath("//button[text()='Sign Out']"));
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
     }
 
     public void logout() {
         click(By.xpath("//button[text()='Sign Out']"));
+    }
+
+    public boolean isAlertPresent(String message) {
+        Alert alert = new WebDriverWait(wd, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert != null && alert.getText().contains(message)) {
+           // System.out.println(alert.getText());
+            //click OK -->alert.accept();
+            // click cancel -->alert.dismiss();
+            //type into alert -->alert.sendKeys("text");
+            //pause(2000);
+            alert.accept();
+            return true;
+        }
+        return false;
     }
 }
