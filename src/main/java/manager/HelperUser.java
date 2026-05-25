e246ac8f44a65d334878ca0c53707ceb341971a3
Mariia Zakharevich <z0559882272@gmail.com>
2026-05-25 20:20:51 +0300

    added WebDriverListener

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -21,6 +21,7 @@ public class HelperUser extends HelperBase {
 //        WebElement loginTab = wd.findElement(By.xpath("//a[text()='LOGIN']"));
 //        loginTab.click();
         click(By.cssSelector("a[href='/login']"));
+        logger.info("open form by click on button with locator a[href='/login']");
     }
 
     public void fillLoginRegistrationForm(String email, String password) {
@@ -29,6 +30,7 @@ public class HelperUser extends HelperBase {
 //        emailInput.clear();
 //        emailInput.sendKeys(email);
         type(By.name("email"), email);
+        logger.info("type in input with locator By.name(\"email\") ");
 
 
 //        WebElement passwordInput = wd.findElement(By.xpath("//input[@placeholder='Password']"));
@@ -36,6 +38,7 @@ public class HelperUser extends HelperBase {
 //        passwordInput.clear();
 //        passwordInput.sendKeys(password);
         type(By.xpath("//input[@placeholder='Password']"), password);
+        //type(By.xpath("//input[@placeholder='Passwordddd']"), password);
     }
 
     public void fillLoginRegistrationForm(User user) {
