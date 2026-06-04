40def78f3db63a91c1f0929b248ef85e796ad64b
Mariia Zakharevich <z0559882272@gmail.com>
2026-06-04 19:39:46 +0300

    added browser & suite

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import manager.ApplicationManager;
 import manager.TestNGListener;
+import org.openqa.selenium.remote.Browser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
@@ -15,7 +16,8 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    static ApplicationManager app = new ApplicationManager();
+    static ApplicationManager app = new ApplicationManager
+            (System.getProperty("browser", Browser.CHROME.browserName()));
 
     @BeforeSuite
     public void setUp() {
@@ -33,7 +35,7 @@ public class TestBase {
     }
 
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
